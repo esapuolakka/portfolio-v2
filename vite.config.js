4c02a4948a233006a9f7b6201fd3c13cc40fef4e
esapuolakka <esa.puolakka@gmail.com>
2024-03-17 16:12:25 +0200

    github actions config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/",
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    base: '/',
+    plugins: [react()],
+  }
+
+  if (command !== 'serve') {
+    config.base = '/portfolio-v2/'
+  }
+  return config;
 })
