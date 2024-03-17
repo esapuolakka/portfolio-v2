63d50e2d431029643bb9ad20b4864413af3cd0fe
esapuolakka <esa.puolakka@gmail.com>
2024-03-17 20:57:38 +0200

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 
 import React from 'react';
-// import Bookstore from './Bookstore/Bookstore';
-// import SignUp from './Bookstore/Signup';
-// import LogIn from './Bookstore/Login';
+import Bookstore from './Bookstore/Bookstore';
+import SignUp from './Bookstore/Signup';
+import LogIn from './Bookstore/Login';
 import Homepage from './Homepage/Homepage';
 import MovieTheater from './MovieTheater/MovieTheater';
 import Trivia from './Trivia/Trivia';
-// import { AuthProvider } from './Contexts/AuthContext';
+import { AuthProvider } from './Contexts/AuthContext';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import ProtectedRoutes from './ProtectedRoutes';
+import ProtectedRoutes from './ProtectedRoutes';
 
 export default function App() {
 
   return (
     <div className='app'>
       <Router basename={import.meta.env.DEV ? '/' : '/portfolio-v2/'}>
-        {/* <AuthProvider> */}
+        <AuthProvider>
           <Routes>
             <Route path="/" element={<Homepage/>}/>
-            {/* <Route path='/signup' element={<SignUp/>} />
+            <Route path='/signup' element={<SignUp/>} />
             <Route path='/login' element={<LogIn/>} />
             <Route path='/bookstore' element={
               <ProtectedRoutes>
                 <Bookstore />
-              </ProtectedRoutes>} /> */}
+              </ProtectedRoutes>} />
             <Route path="/movietheater" element={<MovieTheater />} />
             <Route path="/trivia" element={<Trivia />}/>
           </Routes>
-        {/* </AuthProvider> */}
+        </AuthProvider>
       </Router>
     </div>
   )
--- src/Contexts/AuthContext.jsx
+++ src/Contexts/AuthContext.jsx
@@ -1,60 +1,60 @@
-// import React, { useContext, useEffect, useState } from 'react';
-// import { auth } from '../Firebase';
-// import {
-//   createUserWithEmailAndPassword,
-//   signInWithEmailAndPassword,
-//   onAuthStateChanged,
-//   signOut,
-//   deleteUser } from 'firebase/auth';
+import React, { useContext, useEffect, useState } from 'react';
+import { auth } from '../Firebase';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  deleteUser } from 'firebase/auth';
 
-// const AuthContext = React.createContext();
+const AuthContext = React.createContext();
 
-// export function useAuth() {
-//   return useContext(AuthContext);
-// }
+export function useAuth() {
+  return useContext(AuthContext);
+}
 
-// export function AuthProvider({ children }) {
-//   const [currentUser, setCurrentUser] = useState();
-//   const [loading, setLoading] = useState(true);
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
 
-//   function signup(email, password) {
-//     return createUserWithEmailAndPassword(auth, email, password);
-//   }
+  function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
 
-//   function login(email, password) {
-//     return signInWithEmailAndPassword(auth, email, password);
-//   }
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
 
-//   function signout() {
-//     return signOut(auth);
-//   }
+  function signout() {
+    return signOut(auth);
+  }
 
-//   function deleteCurrentUser() {
-//     return deleteUser(auth.currentUser);
-//   }
+  function deleteCurrentUser() {
+    return deleteUser(auth.currentUser);
+  }
 
-//   useEffect(() => {
-//     const unsubscribe = onAuthStateChanged(auth, user => {
-//       setCurrentUser(user);
-//       setLoading(false)
-//     });
-//     return unsubscribe;
-//   }, []);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setCurrentUser(user);
+      setLoading(false)
+    });
+    return unsubscribe;
+  }, []);
 
 
-//   const value = {
-//     currentUser,
-//     signup,
-//     login,
-//     signout,
-//     deleteCurrentUser
-//   };
+  const value = {
+    currentUser,
+    signup,
+    login,
+    signout,
+    deleteCurrentUser
+  };
 
-//   return (
-//     <div>
-//       <AuthContext.Provider value={value}>
-//         {!loading && children}
-//       </AuthContext.Provider>
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <AuthContext.Provider value={value}>
+        {!loading && children}
+      </AuthContext.Provider>
+    </div>
+  );
+}
